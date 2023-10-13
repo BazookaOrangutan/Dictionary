c29852e03419ede8af7d3d5f2999474ad1e78b08
BazookaOrangutan <104569338+BazookaOrangutan@users.noreply.github.com>
2023-10-13 13:39:36 +0300

    Add files via upload

---
+++ Dictionaries/Dictionaries/LanguageDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    internal class LanguageDictionary<W>
+    {
+        private Dictionary<W, List<string>> dictionary = new Dictionary<W, List<string>>();
+        public void AddWord(W word, List<string> translate)
+        {
+            dictionary[word] = translate; 
+        }
+    }
+}
---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,12 @@
+﻿var dict = new Dictionary<int, object>()
+{
+    {1, "Tom"}
+};
+
+dict[1] = 100;
+
+foreach (var kvp in dict)
+{
+    Console.WriteLine(kvp);
+}
+
