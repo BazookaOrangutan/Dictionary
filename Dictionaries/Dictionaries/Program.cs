026014a63a39d0bd94c74028bb5af12b762dcdfb
BazookaOrangutan <104569338+BazookaOrangutan@users.noreply.github.com>
2023-10-28 01:35:51 +0300

    c6

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -2,133 +2,119 @@
 using System.IO;
 using System.Xml.Linq;
 
-//var dict = new Dictionary<int, List<string>>()
-//{
-//    {1, new List<string>{"Tom", "Tim" } },
-//    {2, new List<string>{"Tom", "Tim" }},
-//    {3, new List<string>{"Tom", "Tim" }}
-//};
 
-//dict[4].Add("1");
-//foreach (var item in dict[1])
+
+//while (true)
 //{
-//    Console.WriteLine(item);
+
+//    Console.Clear();
+//    Console.WriteLine("Введите название словаря, чтобы открыть существующий или создать новый");
+//    var path = Console.ReadLine() + ".xml";
+//    if (!dictionariesCollection.Contains(path))
+//        XmlHelper.CreateXml(path);
+//    while (flag)
+//    {
+//        Console.Clear();
+//        Console.WriteLine($"Словарь {path}");
+//    }
+
 //}
 
-//foreach (var kvp in dict)
-//{
-//    Console.WriteLine(kvp);
-//}
-
-//Console.WriteLine(dict[2].Count);
-
-//XDocument xDoc = new XDocument();
-
-////xDoc = XDocument.Load("people.xml");
-//XElement diction = new XElement("dictionary");
-//XAttribute type = new XAttribute("type", "dwdwdw");
-//diction.Add(type);
-//XElement dicts = new XElement("dicts");
-//dicts.Add(diction);
-//xDoc.Add(dicts);
-//xDoc.Save("dicts.xml");
-//Console.ReadKey();
-
-//var dictionary = new LanguageDictionary("test1");
-//var xdoc = XDocument.Load("test1.xml");
-//var elem1 = new XElement("elem1", "elemName");
-//var dictionaryElement = xdoc.Element("dictionary");
-//dictionaryElement?.Add(elem1);
-//xdoc.Save("test1.xml");
-
-//var xdoc = XDocument.Load("test1.xml");
-//var dictionaryElement = xdoc.Element("dictionary");
-//XElement wordElement;
-//// Если word еще нет в файле
-
-//if (dictionaryElement?.Elements("word").Count() == 0)
-//{
-//    wordElement = new XElement("word");
-//    dictionaryElement?.Add(wordElement);
-//}
-//// Если есть
-//else
-//{
-//    wordElement = dictionaryElement.Element("word");
-//}
-//wordElement?.Add(new XElement("translate", "rrrr"));
-//xdoc.Save("test1.xml");
 
 
-//var xdoc = XDocument.Load("test1.xml");
-//var dictionaryElement = xdoc.Element("dictionary");
-//var wordElement = dictionaryElement.Element("word");
-//if (wordElement != null)
-//{
-//    wordElement.Remove();
-//}
-//xdoc.Save("test1.xml");
-
-//var xdoc = XDocument.Load("test1.xml");
-//var dictionaryElement = xdoc.Element("dictionary");
-//var oldWordElement = dictionaryElement?.Element("word228");
-//if (oldWordElement != null)
-//    oldWordElement.Name = "word";
-//xdoc.Save("test1.xml");
-
-//var xdoc = XDocument.Load("test1.xml");
-//var dictionaryElement = xdoc.Element("dictionary");
-//var wordElement = dictionaryElement?.Element("word");
-//wordElement?.Elements("translate")?
-//    .Where(t => t.Value == "car")
-//    .Remove();
-//xdoc.Save("test1.xml");
-
-//XmlHelper.ReplaceTranslate("test1.xml", "word", "rrrr", "fffff");
-//XmlHelper.AddWord("test1.xml", "word2", "translate1");
-//XmlHelper.RemoveTranslate("test1.xml", "word", "rrrr");
-//XmlHelper.RemoveWord("test1.xml", "elem1");
-//XmlHelper.ReplaceWord("test1.xml", "word", "word1");
-//XmlHelper.RemoveTranslate("test1.xml", "word2", "translate1");
-//XmlHelper.ReplaceTranslate("test1.xml", "word1", "tra222", "translate1");
-
-//XmlHelper.AddWord("test1.xml", "word1", "translate2");
-//XmlHelper.AddWord("test1.xml", "word1", "translate3");
-//XmlHelper.AddWord("test1.xml", "word2", "translate");
-
-//Console.WriteLine(XmlHelper.Search("test1.xml", "word1"));
-
-//XmlHelper.ReplaceWord("test1.xml", "word", "word1");
-//var path = "test1.xml";
-//XmlHelper.RemoveWord(path, "word1");
-
-//XmlHelper.AddWord(path, "word1", "translate1");
-//XmlHelper.AddWord(path, "word1", "translate2");
-//XmlHelper.AddWord(path, "word1", "translate3");
-
-//XmlHelper.AddWord(path, "word2", "translate1");
-//XmlHelper.AddWord(path, "word2", "translate4");
-//XmlHelper.AddWord(path, "word2", "translate5");
-
-//XmlHelper.ReplaceWord(path, "word1", "word2");
 var dictionariesXml = new XDocument("dictionaries.xml");
 dictionariesXml?.Add(new XElement("dictionaries"));
+var dictionariesElement = dictionariesXml?.Element("dictioanaries");
 var dictionariesCollection = new List<string>();
-foreach(var dictionarie in dictionariesXml?.Element("dictionaries")?.Elements())
+foreach (var dictionarie in dictionariesXml?.Element("dictionaries")?.Elements())
 {
     dictionariesCollection.Add(dictionarie.Value);
 }
+dictionariesCollection.Add("Выход");
+var dcLength = dictionariesCollection?.Count ?? 0;
 var flag = false;
+string path;
+var menuItems = new List<string> { "1.Добавить перевод", "2.Удалить слово из словаря", "3.Изменить слово", 
+    "4.Удалить один из переводов слова", "5.Изменить перевод слова", "6.Найти перевод слова", "Назад" };
+
+Console.WriteLine("Меню");
+Console.WriteLine();
+
+int row = Console.CursorTop;
+int col = Console.CursorLeft;
+int index = 0;
 while (true)
 {
-    if (flag)
+    while (flag)
     {
-        Console.Clear();
+        DrawMenu(menuItems, row, col, index);
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.DownArrow:
+                if (index < menuItems.Count - 1)
+                    index++;
+                break;
+            case ConsoleKey.UpArrow:
+                if (index > 0)
+                    index--;
+                break;
+            case ConsoleKey.Enter:
+                switch (index)
+                {
+                    case 6:
+                        Console.WriteLine("Выбран выход из приложения");
+                        //return;
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine($"Выбран пункт {menuItems[index]}");
+                        break;
+                }
+                break;
+        }
     }
-    Console.Clear();
-    Console.WriteLine("Введите название словаря, чтобы открыть существующий или создать новый");
-    var path = Console.ReadLine()+".xml";
-    if(!dictionariesCollection.Contains(path))
-        XmlHelper.CreateXml(path);
+    DrawMenu(dictionariesCollection, row, col, index);
+    switch (Console.ReadKey(true).Key)
+    {
+        case ConsoleKey.DownArrow:
+            if (index < dictionariesCollection.Count - 1)
+                index++;
+            break;
+        case ConsoleKey.UpArrow:
+            if (index > 0)
+                index--;
+            break;
+        case ConsoleKey.Enter:
+            if (index == dictionariesCollection.Count - 1) return;
+            else if (index == 0)
+            {
+                Console.WriteLine("Введите название словаря: ");
+                path = Console.ReadLine() + ".xml";
+                dictionariesCollection.Add(path);
+                dictionariesElement?.Add(new XElement(path));
+                flag = true;
+            }
+            else
+            {
+                flag = true;
+                path = dictionariesCollection[index];
+            }
+            break;
 
+    }
+}
+static void DrawMenu(List<string> items, int row, int col, int index)
+{
+    Console.SetCursorPosition(col, row);
+    for (int i = 0; i < items.Count; i++)
+    {
+        if (i == index)
+        {
+            Console.BackgroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        Console.WriteLine(items[i]);
+        Console.ResetColor();
+    }
+    Console.WriteLine();
 }
